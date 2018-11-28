f3a5ed3ee9bc7f856b40dbd1a1b7f27b975ca971
krobawsky <richibq20@gmail.com>
2018-11-28 18:18:07 -0500

    init

---
+++ PlayBot2/PlayBot2.Android/GilActivity.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Bluetooth;
+using System.IO;
+using Java.Util;
+using System.Threading.Tasks;
+using Java.Lang;
+
+namespace PlayBot2.Droid
+{
+    [Activity(Label = "PlayBot", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    class GilActivity : AppCompatActivity
+    {
+
+        private Java.Lang.String dataToSend;
+
+        private BluetoothAdapter btAdapter = null;
+        private BluetoothSocket btSocket = null;
+
+        private Stream outStream = null;
+        private Stream inStream = null;
+
+        private static readonly UUID MY_UUID = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            RequestWindowFeature(WindowFeatures.IndeterminateProgress);
+            SetContentView(Resource.Layout.control);
+
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            string address = Intent.GetStringExtra("mac");
+            string name = Intent.GetStringExtra("name");
+
+            SupportActionBar.Title = name;
+
+            Toast.MakeText(this, address, ToastLength.Short).Show();
+
+            CheckBt();
+            Connect(address);
+            
+        }
+
+        private void CheckBt()
+        {
+            btAdapter = BluetoothAdapter.DefaultAdapter;
+
+            if (!btAdapter.Enable())
+            {
+                Toast.MakeText(this, "Bluetooth Desactivado", ToastLength.Short).Show();
+            }
+            if (btAdapter == null)
+            {
+                Toast.MakeText(this, "Bluetooth No Existe o esta Ocupado", ToastLength.Short).Show();
+            }
+        }
+
+        public void Connect(string address)
+        {
+
+            if (address.Equals(""))
+            {
+                Toast.MakeText(this, "Direccion mac no encontrada, intentelo de nuevo.", ToastLength.Short).Show();
+                StartActivity(typeof(MainActivity));
+                Finish();
+            }
+
+            BluetoothDevice device = null;
+            try
+            {
+                device = btAdapter.GetRemoteDevice(address);
+                Console.WriteLine("Conexion en curso" + device);
+            }
+            catch (IllegalArgumentException e)
+            {
+                Console.WriteLine(e);
+                Toast.MakeText(this, "La direccion mac " + address + " es incorrecta, intentelo de nuevo.", ToastLength.Short).Show();
+                StartActivity(typeof(MainActivity));
+                Finish();
+            }
+
+            // btAdapter.CancelDiscovery();
+            try
+            {
+                btSocket = device.CreateRfcommSocketToServiceRecord(MY_UUID);
+                btSocket.Connect();
+                Toast.MakeText(this, "Conexion Correcta", ToastLength.Short).Show();
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Toast.MakeText(this, "Error en la conexion", ToastLength.Short).Show();
+
+                StartActivity(typeof(MainActivity));
+                Finish();
+
+                try
+                {
+                    btSocket.Close();
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Imposible Conectar");
+                }
+                Console.WriteLine("Socket Creado");
+            }
+        }
+
+        public void beginListenForData()
+        {
+            try
+            {
+                inStream = btSocket.InputStream;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Task.Factory.StartNew(() => {
+
+                byte[] buffer = new byte[1024];
+                int bytes;
+                while (true)
+                {
+                    try
+                    {
+                        bytes = inStream.Read(buffer, 0, buffer.Length);
+                        if (bytes > 0)
+                        {
+                            RunOnUiThread(() => {
+                                string valor = Encoding.ASCII.GetString(buffer);
+                                Console.WriteLine("Datos recividos: " + valor);
+                                Toast.MakeText(this, valor, ToastLength.Short).Show();
+                            });
+                        }
+                    }
+                    catch (Java.IO.IOException)
+                    {
+                        RunOnUiThread(() => {
+                            Console.WriteLine("Error al recivir datos");
+                        });
+                        break;
+                    }
+                }
+            });
+        }
+
+        private void writeData(Java.Lang.String data)
+        {
+            try
+            {
+                outStream = btSocket.OutputStream;
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Error al enviar" + e.Message);
+            }
+
+            Java.Lang.String message = data;
+            byte[] msgBuffer = message.GetBytes();
+
+            try
+            {
+                outStream.Write(msgBuffer, 0, msgBuffer.Length);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Error al enviar" + e.Message);
+            }
+        }
+    }
+}
---
+++ PlayBot2/PlayBot2.Android/MainActivity.cs
@@ -0,0 +1,227 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Bluetooth;
+using Android.Content;
+using Android;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
+using Android.Support.V7.App;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace PlayBot2.Droid
+{
+    [Activity(Label = "PlayBot", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : AppCompatActivity
+    {
+        
+        private BluetoothAdapter btAdapter = null;
+        static ArrayAdapter<string> newDevicesArrayAdapter;
+        static ArrayAdapter<string> pairedDevicesArrayAdapter;
+        private DeviceDiscoveredReceiver receiver;
+
+        private Button devButton;
+        private Button scanButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            RequestWindowFeature(WindowFeatures.IndeterminateProgress);
+            SetContentView(Resource.Layout.main);
+
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = GetString(Resource.String.dev_pair);
+
+            permissions();
+            CheckBt();
+
+            SetResult(Result.Canceled);
+
+            newDevicesArrayAdapter = new ArrayAdapter<string>(this, Resource.Layout.device_name);
+            pairedDevicesArrayAdapter = new ArrayAdapter<string>(this, Resource.Layout.device_name);
+            
+            var DevicesListView = FindViewById<ListView>(Resource.Id.list_devices);
+            DevicesListView.ItemClick += DeviceListView_ItemClick;
+
+            devButton = FindViewById<Button>(Resource.Id.btn_dev);
+            devButton.Click += (sender, e) =>
+            {
+                SupportActionBar.Title = GetString(Resource.String.dev_pair);
+                DevicesListView.Adapter = pairedDevicesArrayAdapter;
+                pairedDevicesArrayAdapter.Clear();
+
+                DevicePair();
+                (sender as View).Visibility = ViewStates.Gone;
+                scanButton.Visibility = ViewStates.Visible;
+            };
+
+
+            scanButton = FindViewById<Button>(Resource.Id.btn_scan);
+            scanButton.Click += (sender, e) =>
+            {
+                SupportActionBar.Title = GetString(Resource.String.dev_search);
+                DevicesListView.Adapter = newDevicesArrayAdapter;
+                newDevicesArrayAdapter.Clear();
+
+                DoDiscovery();
+                (sender as View).Visibility = ViewStates.Gone;
+            };
+
+            DevicesListView.Adapter = pairedDevicesArrayAdapter;
+            DevicePair();
+
+            receiver = new DeviceDiscoveredReceiver(this, devButton);
+
+            var filter = new IntentFilter(BluetoothDevice.ActionFound);
+            RegisterReceiver(receiver, filter);
+
+            filter = new IntentFilter(BluetoothAdapter.ActionDiscoveryFinished);
+            RegisterReceiver(receiver, filter);
+
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            
+            if (btAdapter != null)
+            {
+                btAdapter.CancelDiscovery();
+            }
+            UnregisterReceiver(receiver);
+        }
+
+        private void CheckBt()
+        {
+            btAdapter = BluetoothAdapter.DefaultAdapter;
+
+            if (!btAdapter.Enable())
+            {
+                Toast.MakeText(this, "Bluetooth Desactivado", ToastLength.Short).Show();
+            }
+            if (btAdapter == null)
+            {
+                Toast.MakeText(this, "Bluetooth No Existe o esta Ocupado", ToastLength.Short).Show();
+            }
+        }
+
+        void DevicePair()
+        {
+            if (btAdapter.IsDiscovering)
+            {
+                btAdapter.CancelDiscovery();
+            }
+
+            var pairedDevices = btAdapter.BondedDevices;
+
+            if (pairedDevices.Count > 0)
+            {
+                foreach (var dev in pairedDevices)
+                {
+                    pairedDevicesArrayAdapter.Add(dev.Name + "\n" + dev.Address);
+                }
+            }
+        }
+
+        void DoDiscovery()
+        {
+            if (btAdapter.IsDiscovering)
+            {
+                btAdapter.CancelDiscovery();
+            }
+            btAdapter.StartDiscovery();
+            //progress(true);
+        }
+
+        void DeviceListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            //btAdapter.CancelDiscovery();
+            var info = ((TextView)e.View).Text;
+            var address = info.Substring(info.Length - 17);
+            var name = info.Substring(0, info.Length - 17);
+
+            Android.App.AlertDialog.Builder dialog = new Android.App.AlertDialog.Builder(this);
+            Android.App.AlertDialog alert = dialog.Create();
+            alert.SetTitle("PlayBot");
+            alert.SetMessage("Desea conectarse con el dispositivo " + name);
+            alert.SetButton("OK", (c, ev) =>
+            {
+                var activity = new Intent(this, typeof(GilActivity));
+                activity.PutExtra("name", name);
+                activity.PutExtra("mac", address);
+                StartActivity(activity);
+            });
+            alert.Show();
+            
+        }
+
+        public class DeviceDiscoveredReceiver : BroadcastReceiver
+        {
+            Activity activity;
+            Button devButton;
+
+            public DeviceDiscoveredReceiver(Activity activity, Button button)
+            {
+                this.activity = activity;
+                devButton = button;
+            }
+
+            public override void OnReceive(Context context, Intent intent)
+            {
+                string action = intent.Action;
+                
+                if (action == BluetoothDevice.ActionFound)
+                {
+                    BluetoothDevice device = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
+                    if (device.BondState != Bond.Bonded)
+                    {
+                        newDevicesArrayAdapter.Add(device.Name + "\n" + device.Address);
+                    }
+                }
+                else if (action == BluetoothAdapter.ActionDiscoveryFinished)
+                {
+                    Console.WriteLine("Finish");
+                    Toast.MakeText(activity, "Busqueda finalizada", ToastLength.Short).Show();
+                    devButton.Visibility = ViewStates.Visible;
+                }
+            }
+        }
+
+        public void progress(bool x)
+        {
+            var progress = new ProgressDialog(this);
+            if (x)
+            {
+                progress.Indeterminate = true;
+                progress.SetProgressStyle(ProgressDialogStyle.Spinner);
+                progress.SetMessage("Loading is Progress...");
+                progress.SetCancelable(false);
+                progress.Show();
+            }
+            else
+            {
+                progress.Dismiss();
+            }
+            
+        }
+
+        public void permissions()
+        {
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.AccessCoarseLocation, Manifest.Permission.AccessFineLocation,
+                    Manifest.Permission.Bluetooth, Manifest.Permission.BluetoothAdmin}, 0);
+            }
+            else
+            {
+                Console.WriteLine("Permission Granted!!!");
+            }
+        }
+    }
+}
